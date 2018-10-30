97fda7d87ca456237e3cf3a9f584957b320bdc1a
dalsian <dalsian.mung@gmail.com>
2018-10-30 12:36:04 -0500

    .

--- src/server/routes/notification-api.js
+++ src/server/routes/notification-api.js
@@ -36,6 +36,7 @@ const updateValidation = {
   }
 };
 
+
 /* create APN provider for push notifications */
 const options = {
   cert: path.join(appRoot, config.KEYS_PATH, config.CERT_FILE),
@@ -44,6 +45,22 @@ const options = {
   production: true
 };
 const apnProvider = new apn.Provider(options);
+
+const validateToken = (req, res, next) => {
+  const authorization = req.get('Authorization');
+  if (authorization) {
+    const token = authorization.split(' ')[1] || authorization.split(' ')[0];
+    const decoded = Buffer.from(token, 'base64').toString('ascii');
+    bcrypt.compare(config.TOKEN_SECRET, decoded, (err, match) => {
+      logger.info(`Match ${match}`);
+      if (err) next({ status: 401, message: `Authorization: ${err}` });
+      else if (!match) next({ status: 401, message: 'Authorization failure: auth token doesn\'t match' });
+      else next();
+    });
+  } else {
+    next({ status: 401, message: `Authorization NOT found in header for ${req.url}` });
+  }
+};
 
 /*
  * Update pass fields and send Push Notification
