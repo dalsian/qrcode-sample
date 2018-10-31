0ca2f2471a0cac2b94b1c9112fc78e73aa5b0664
dalsian <dalsian.mung@gmail.com>
2018-10-31 17:55:43 -0500

    .

--- src/server/services/7-rewards-consumer.js
+++ src/server/services/7-rewards-consumer.js
@@ -43,6 +43,7 @@ const getClientToken = () => {
         path: '/auth/token',
         method: 'POST',
         headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
             'Content-Length': Buffer.byteLength(querystring.stringify((postData)))
         }
     };
